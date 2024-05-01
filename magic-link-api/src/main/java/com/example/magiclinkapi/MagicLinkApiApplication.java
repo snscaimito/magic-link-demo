3cf11036b9e628fde54cc6925d08e44419546a6d
Stephan Schwab <sns@caimito.net>
2024-05-01 15:43:52 +0200

    basic infrastructure

---
+++ magic-link-api/src/main/java/com/example/magiclinkapi/MagicLinkApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.magiclinkapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagicLinkApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagicLinkApiApplication.class, args);
+	}
+
+}
---
+++ magic-link-api/src/test/java/com/example/magiclinkapi/MagicLinkApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.magiclinkapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagicLinkApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
