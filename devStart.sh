43642c7126c78255cb29940609c0db1ea74cfdf3
Stephan Schwab <sns@caimito.net>
2024-05-03 10:25:57 +0200

    mouse scrolling in tmux

--- devStart.sh
+++ devStart.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-export API_HOST=`ifconfig en0 | grep inet | awk '$1=="inet" {print $2}'`
+
+export API_HOST=$(ifconfig en0 | grep inet | awk '$1=="inet" {print $2}')
 
 tmux new-session -d -s magic-link-demo 'cd magic-link-api; ./mvnw spring-boot:run; read'
-tmux split-window -h -t magic-link-demo 'docker compose --profile development up magic-link-web-dev --build; read'
+tmux new-window -t magic-link-demo:1 'docker compose --profile development up magic-link-web-dev --build; read'
+
+# Enable mouse support
+tmux set-option -g mouse on
+
 tmux attach-session -t magic-link-demo
